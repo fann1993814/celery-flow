f68752c6c3ab1d6b0ac3df75f839036d214f4452
Jason Fan <fann1993814@gmail.com>
2023-11-14 15:04:03 +0800

    update queue name

--- manerger/main.py
+++ manerger/main.py
@@ -5,11 +5,22 @@ app = Celery('tasks')
 # load config
 app.config_from_object('celeryconfig')
 
-chained_tasks = chain([
-    Signature('worker1.add', kwargs={"x": 2, 'y': 3}, queue='worker'),
-    Signature('worker2.mul', kwargs={'y': 5}, queue='worker'),
-])
 
+task_add = Signature('worker1.add', kwargs={"x": 2, 'y': 3}, queue='worker1')
+res = task_add.apply_async()
+
+print(res.get())
+
+task_mul = Signature('worker2.mul', kwargs={
+                     'x': 5, 'y': 10}, queue='worker2')
+res = task_mul.apply_async()
+
+print(res.get())
+
+chained_tasks = chain([
+    Signature('worker1.add', kwargs={"x": 2, 'y': 3}, queue='worker1'),
+    Signature('worker2.mul', kwargs={'y': 5}, queue='worker2'),
+])
 
 res = chained_tasks.apply_async()
 
